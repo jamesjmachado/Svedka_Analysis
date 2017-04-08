f431afaa3b1a477a2d6d2b5585ac1335d7cd6423
James Machado <jmachado@brandeis.edu>
2017-04-08 16:51:26 -0400

    updates to code as of 4:51pm on Saturday 4.8.17

--- Svedka Analysis Script.R
+++ Svedka Analysis Script.R
@@ -14,7 +14,7 @@ View(Svedka_data)
 # James: My csv is giving me a bunch of unecessary columns at the end, so I need to remove them.
 # You may not need to do this if you use your own csv file
 Svedka_data = subset(Svedka_data, select = -c(X68, X69, X70, X71, X72, X73, X74, X75) )
-# There is a typo in the PricePerUnit column -- the column in listed as PriceRerUnit
+# There is a typo in the PricePerUnit column -- the column in listed as 'PriceRerUnit'
 library(plyr)
 Svedka_data <- rename(Svedka_data, c("PriceRerUnit"="PricePerUnit"))
 
@@ -22,8 +22,23 @@ Svedka_data <- rename(Svedka_data, c("PriceRerUnit"="PricePerUnit"))
 # Comment on the shape of the distribution that you see.
 
 hist(Svedka_data$TotalSales,
-     main = 'Histogram of Combined Total Sales for Featured Vodka Brands Between 1995-2007',
+     main = 'Hist. of Combined Total Sales for \nFeatured Vodka Brands (1995-2007)',
        xlab = 'Total Sales')
+
+#Export Histograms for Jonny
+# pdf
+pdf("TotalSalesHist.pdf")
+hist(Svedka_data$TotalSales,
+     main = 'Hist. of Combined Total Sales for \nFeatured Vodka Brands (1995-2007)',
+     xlab = 'Total Sales')
+dev.off()
+
+#png
+png("TotalSalesHist.png")
+hist(Svedka_data$TotalSales,
+     main = 'Hist. of Combined Total Sales for \nFeatured Vodka Brands (1995-2007)',
+     xlab = 'Total Sales')
+dev.off()
 
 # The shape of the distribution is extremely downward trending with the majority 
 # or Total Sales at $2,000 or less.
@@ -38,6 +53,14 @@ hist(Svedka_data$TotalSales,
 RegQ2 <- lm(LnSales ~ PricePerUnit + Print + Outdoor + Broad + LagTotalSales, data = Svedka_data)
 summary(RegQ2)
 
+# Export Regression Summary to .csv file
+library(devtools)
+# install_github("dgrtwo/broom")
+library(broom)
+Q2RegSummary <- tidy(RegQ2)
+Q2RegSummary
+write.csv(Q2RegSummary, "Q2RegSummary.csv")
+
 # I don't know the answer to this question.
 
 # 3. (15) Sometimes we can improve model fit by taking logs on independent variables. 
@@ -49,6 +72,14 @@ summary(RegQ2)
 RegQ3 <- lm((LnSales-LnLSales) ~ PricePerUnit + LnPrint + LnOut + LnBroad, data = Svedka_data)
 summary(RegQ3)
 
+# Export Regression Summary to .csv file
+library(devtools)
+library(broom)
+Q3RegSummary <- tidy(RegQ3)
+Q3RegSummary
+write.csv(Q3RegSummary, "Q3RegSummary.csv")
+
+
 # Not sure about the RegQ3 code above
 # I don't know the answer to this question.
 
@@ -58,19 +89,47 @@ summary(RegQ3)
 # whether a vodka brand belongs to the first or second quality tiers) to the set of 
 # independent variables named in question 2.  How does quality influence sales?
 
-#Stuck Here
 RegQ4 <- lm(LnSales ~ PricePerUnit + Print + Outdoor + Broad + LagTotalSales + Tier1 + Tier2, data = Svedka_data)
-#Stuck Here
+summary(RegQ4)
+
+# Export Regression Summary to .csv file
+library(devtools)
+library(broom)
+Q4RegSummary <- tidy(RegQ4)
+Q4RegSummary
+write.csv(Q4RegSummary, "Q4RegSummary.csv")
+
 
 # 5. (15) To understand the influence of competition and brand power, expand your model 
 # again and run a regression by adding the sum of sales of all the competing brands in 
 # the previous year (“lagtotalminussales”) to the independent variables in question 3. 
 # What additional insight does this model provide?
 
+RegQ5 <- lm((LnSales-LnLSales) ~ PricePerUnit + LnPrint + LnOut + LnBroad + LagTotalMinusSales, data = Svedka_data)
+summary(RegQ5)
+
+# Export Regression Summary to .csv file
+library(devtools)
+library(broom)
+Q5RegSummary <- tidy(RegQ5)
+Q5RegSummary
+write.csv(Q5RegSummary, "Q5RegSummary.csv")
+
+
 # 6.	(10) To measure the sales growth of new brands compared to the existent ones, 
 # include the variable “firstintro” to the independent variable set in question 4. 
 # “Firstintro” is equal to 1 in the first three years after a brand is introduced, 
 # and equals 0 elsewhere.  How does it help to include this variable in the model?
+
+RegQ6 <- lm(LnSales ~ PricePerUnit + Print + Outdoor + Broad + LagTotalSales + Tier1 + Tier2 + Firstintro, data = Svedka_data)
+summary(RegQ6)
+
+# Export Regression Summary to .csv file
+library(devtools)
+library(broom)
+Q6RegSummary <- tidy(RegQ6)
+Q6RegSummary
+write.csv(Q6RegSummary, "Q6RegSummary.csv")
 
 
 # 7.	(10) Examine the coefficients of the price and advertising variables in your 
